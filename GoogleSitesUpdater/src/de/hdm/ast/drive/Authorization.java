c47167972e9ff8b596fa7c83b76c6199e772b873
Randalinho <markusschmieder@gmx.de>
2015-02-16 16:03:34 +0100

    erste klasse

---
+++ GoogleSitesUpdater/src/de/hdm/ast/drive/Authorization.java
@@ -0,0 +1,15 @@
+package de.hdm.ast.drive;
+
+//import com.google.api.client.googleapis.extensions.appengine.auth.oauth2.AppIdentityCredential;
+//import com.google.api.services.sqladmin.SQLAdminScopes;
+//
+//// ...
+//
+//AppIdentityCredential credential =
+//    new AppIdentityCredential(SQLAdminScopes.SQLSERVICE_ADMIN);
+
+
+
+public class Authorization {
+
+}
