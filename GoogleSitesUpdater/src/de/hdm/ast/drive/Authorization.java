355d10a664e58676d3023ebd568c678599a14e9b
Randalinho <markusschmieder@gmx.de>
2015-02-16 16:25:02 +0100

    gitignore Web-inf

--- GoogleSitesUpdater/src/de/hdm/ast/drive/Authorization.java
+++ GoogleSitesUpdater/src/de/hdm/ast/drive/Authorization.java
@@ -1,12 +1,12 @@
 package de.hdm.ast.drive;
 
-//import com.google.api.client.googleapis.extensions.appengine.auth.oauth2.AppIdentityCredential;
-//import com.google.api.services.sqladmin.SQLAdminScopes;
-//
-//// ...
-//
-//AppIdentityCredential credential =
-//    new AppIdentityCredential(SQLAdminScopes.SQLSERVICE_ADMIN);
+import com.google.api.client.googleapis.extensions.appengine.auth.oauth2.AppIdentityCredential;
+import com.google.api.services.sqladmin.SQLAdminScopes;
+
+// ...
+
+AppIdentityCredential credential =
+    new AppIdentityCredential(SQLAdminScopes.SQLSERVICE_ADMIN);
 
 
 
