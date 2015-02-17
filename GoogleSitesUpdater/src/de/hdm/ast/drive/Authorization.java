eded915d53ab598d1dbe292d1cda060c40288ffd
Randalinho <markusschmieder@gmx.de>
2015-02-17 14:44:26 +0100

    div klassen erstellt

--- GoogleSitesUpdater/src/de/hdm/ast/drive/Authorization.java
+++ GoogleSitesUpdater/src/de/hdm/ast/drive/Authorization.java
@@ -3,12 +3,12 @@ package de.hdm.ast.drive;
 import com.google.api.client.googleapis.extensions.appengine.auth.oauth2.AppIdentityCredential;
 import com.google.api.services.drive.DriveScopes;
 
-
-
-
-
 public class Authorization {
 
-	AppIdentityCredential credential = new AppIdentityCredential(
-			DriveScopes.all());
+	public AppIdentityCredential createCredential() {
+		AppIdentityCredential credential = new AppIdentityCredential(
+				DriveScopes.all());
+		return credential;
+	}
+
 }
