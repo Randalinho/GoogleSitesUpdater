a17fdb0eb67cfcc6884485b3af1e915a55af122b
Randalinho <markusschmieder@gmx.de>
2015-02-17 12:28:26 +0100

    jo

--- GoogleSitesUpdater/src/de/hdm/ast/drive/Authorization.java
+++ GoogleSitesUpdater/src/de/hdm/ast/drive/Authorization.java
@@ -1,15 +1,14 @@
 package de.hdm.ast.drive;
 
 import com.google.api.client.googleapis.extensions.appengine.auth.oauth2.AppIdentityCredential;
+import com.google.api.services.drive.DriveScopes;
 
-//import com.google.api.client.googleapis.extensions.appengine.auth.oauth2.AppIdentityCredential;
-//import com.google.api.services.sqladmin.SQLAdminScopes;
-//
-//// ...
-//
-//AppIdentityCredential credential =
-//    new AppIdentityCredential(SQLAdminScopes.SQLSERVICE_ADMIN);
+
+
+
 
 public class Authorization {
 
+	AppIdentityCredential credential = new AppIdentityCredential(
+			DriveScopes.all());
 }
