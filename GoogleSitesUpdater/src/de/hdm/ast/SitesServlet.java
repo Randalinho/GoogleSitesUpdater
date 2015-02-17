b1841822fb65582355e3d2c7a55eedee5c0abc1a
Randalinho <markusschmieder@gmx.de>
2015-02-18 00:27:08 +0100

    ChronJob: Ausgabe aller Dateien im Drive

--- GoogleSitesUpdater/src/de/hdm/ast/SitesServlet.java
+++ GoogleSitesUpdater/src/de/hdm/ast/SitesServlet.java
@@ -1,6 +1,10 @@
 package de.hdm.ast;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +14,9 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.extensions.appengine.auth.oauth2.AppIdentityCredential;
 import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.Drive.Files;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 
 import de.hdm.ast.drive.Authorization;
 import de.hdm.ast.drive.Service;
@@ -25,15 +32,37 @@ public class SitesServlet extends HttpServlet {
 
 	AppIdentityCredential credentials;
 	Drive drive;
+
 	
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws IOException {
+//		credentials = Authorization.createCredential();
+//		drive = Service.buildService(credentials);
 		
-		credentials = Authorization.createCredential();
-		drive = Service.buildService(credentials);
-		
+		try {
+			drive = Authorization.getDriveService();
+		} catch (GeneralSecurityException | URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-		System.out.println(drive.files().list().size());
+		
+		Files.List request1 = drive.files().list();
+		FileList files = request1.execute();
+
+		if (files != null) {
+			  for (File file : files.getItems()) {
+			    // Meta data
+			    System.out.println("Title: " + file.getTitle());
+			    System.out.println("Description: " + file.getDescription());
+			    System.out.println("MIME type: " + file.getMimeType());
+			    System.out.println("LastModifyingUserName: " + file.getLastModifyingUserName());
+			    System.out.println("ID: " + file.getId());
+			  }
+			}
+		
+		System.out.println("Liste Dateien Anzahl: " + drive.files().list().size());
+		System.out.println("Parents" + drive.getRootUrl());
 		System.out.print("Job wurde erfolgreich ausgeführt");
 	}
 
--- GoogleSitesUpdater/src/de/hdm/ast/drive/Authorization.java
+++ GoogleSitesUpdater/src/de/hdm/ast/drive/Authorization.java
@@ -1,14 +1,45 @@
 package de.hdm.ast.drive;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+
 import com.google.api.client.googleapis.extensions.appengine.auth.oauth2.AppIdentityCredential;
+import com.google.api.client.googleapis.services.CommonGoogleClientRequestInitializer;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
 
 public class Authorization {
 
-	public static AppIdentityCredential createCredential() {
-		AppIdentityCredential credential = new AppIdentityCredential(
-				DriveScopes.all());
-		return credential;
+	// public static AppIdentityCredential createCredential() {
+	// AppIdentityCredential credential = new AppIdentityCredential(
+	// Arrays.asList(DriveScopes.DRIVE));
+	// return credential;
+	// }
+
+	private static final String API_KEY = "key.p12";
+
+	public static Drive getDriveService() throws GeneralSecurityException,
+			IOException, URISyntaxException {
+		HttpTransport httpTransport = new NetHttpTransport();
+		JsonFactory jsonFactory = new JacksonFactory();
+		AppIdentityCredential credential = new AppIdentityCredential.Builder(
+				Arrays.asList(DriveScopes.DRIVE)).build();
+		GoogleClientRequestInitializer keyInitializer = new CommonGoogleClientRequestInitializer(
+				API_KEY);
+		Drive service = new Drive.Builder(httpTransport, jsonFactory, null)
+				.setHttpRequestInitializer(credential)
+				.setGoogleClientRequestInitializer(keyInitializer)
+				.setApplicationName("GoogleSitesUpdater")
+				.build();
+		return service;
+
 	}
 
 }
