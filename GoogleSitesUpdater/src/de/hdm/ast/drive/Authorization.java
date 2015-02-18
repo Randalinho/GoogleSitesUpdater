a7a879392b582041d946c2b246ab30e6bdbed3d7
Randalinho <markusschmieder@gmx.de>
2015-02-18 01:14:43 +0100

    im richtigen Drive

--- GoogleSitesUpdater/src/de/hdm/ast/drive/Authorization.java
+++ GoogleSitesUpdater/src/de/hdm/ast/drive/Authorization.java
@@ -1,11 +1,15 @@
 package de.hdm.ast.drive;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
+import java.util.Collections;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.extensions.appengine.auth.oauth2.AppIdentityCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.googleapis.services.CommonGoogleClientRequestInitializer;
 import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.google.api.client.http.HttpTransport;
@@ -27,13 +31,22 @@ public class Authorization {
 
 	public static Drive getDriveService() throws GeneralSecurityException,
 			IOException, URISyntaxException {
-		HttpTransport httpTransport = new NetHttpTransport();
-		JsonFactory jsonFactory = new JacksonFactory();
-		AppIdentityCredential credential = new AppIdentityCredential.Builder(
-				Arrays.asList(DriveScopes.DRIVE)).build();
+//		HttpTransport httpTransport = new NetHttpTransport();
+//		JsonFactory jsonFactory = new JacksonFactory();
+		JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+		HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+//		AppIdentityCredential credential = new AppIdentityCredential.Builder(
+//				Arrays.asList(DriveScopes.DRIVE)).build();
+		GoogleCredential credential = new GoogleCredential.Builder()
+	    .setTransport(httpTransport)
+	    .setJsonFactory(JSON_FACTORY)
+	    .setServiceAccountId("1094795664207-88e383oaijjuh334qdkr7nf0dvuut32r@developer.gserviceaccount.com")
+	    .setServiceAccountPrivateKeyFromP12File(new File(API_KEY))
+	    .setServiceAccountScopes(Collections.singleton(DriveScopes.DRIVE))
+	    .build();
 		GoogleClientRequestInitializer keyInitializer = new CommonGoogleClientRequestInitializer(
 				API_KEY);
-		Drive service = new Drive.Builder(httpTransport, jsonFactory, null)
+		Drive service = new Drive.Builder(httpTransport, JSON_FACTORY, null)
 				.setHttpRequestInitializer(credential)
 				.setGoogleClientRequestInitializer(keyInitializer)
 				.setApplicationName("GoogleSitesUpdater")
